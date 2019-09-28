1fc01b28c28b166b41825b7a5b6138c3c9d84e1f
haryu703 <34744512+haryu703@users.noreply.github.com>
2019-09-28 15:21:40 +0000

    [server] add covenants

--- hamingja-server/src/controllers/address.controller.ts
+++ hamingja-server/src/controllers/address.controller.ts
@@ -1,10 +1,35 @@
-import {JsonController, Get } from "routing-controllers";
+import { JsonController, Get, QueryParam } from "routing-controllers";
+import { Spedn } from 'spedn';
+import path from 'path';
+
+const network = 'testnet';
 
 @JsonController('/address')
 export class AddressController {
 
   @Get()
-  getAddress() {
-    return '';
+  async getAddress(
+    @QueryParam('owner_pkh', {required: true}) ownerPkh: string,
+    @QueryParam('customer_pkh', {required: true}) customerPkh: string,
+    @QueryParam('max_value', {required: true}) maxValue: number,
+    @QueryParam('token_id', {required: true}) tokenId: string,
+  ) {
+    const compiler = new Spedn();
+    const Covenant = await compiler.compileFile(path.resolve('spedn/covenants.spedn'));
+    compiler.dispose();
+
+    console.log(ownerPkh, customerPkh, maxValue, tokenId);
+
+    const max = Buffer.alloc(8);
+    max.writeBigInt64LE(BigInt(maxValue));
+
+    const instance = new Covenant({
+      ownerPkh: Buffer.from(ownerPkh, 'hex'),
+      customerPkh: Buffer.from(customerPkh, 'hex'),
+      maxValue: max,
+      tokenId: Buffer.from(tokenId, 'hex'),
+    });
+
+    return instance.getAddress(network);
   }
 }
